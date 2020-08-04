0e545cf12c5fb4df0a0447ba1505dfd8490db98c
Hector Diaz <xeroxislabz@gmail.com>
2020-08-04 07:39:20 -0400

    Add image asset cloudy.png Implementation of Date and format, temperature and description

--- lib/ui/MyHomePage.dart
+++ lib/ui/MyHomePage.dart
@@ -1,6 +1,5 @@
-
-
 import 'package:flutter/material.dart';
+import 'package:flutter_weather_app_course/ui/WeatherItems.dart';
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
@@ -24,6 +23,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+
     // This method is rerun every time setState is called, for instance as done
     // by the _incrementCounter method above.
     //
@@ -31,15 +31,13 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
+      backgroundColor: Colors.lightBlue,
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-      ),
+      body: WeatherItems(),
     );
   }
 }
